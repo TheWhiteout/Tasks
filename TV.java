b77516499fda664ada20e313df9e3ccef619b54a
TheWhiteout <72592375+TheWhiteout@users.noreply.github.com>
2020-12-16 15:13:09 +0200

    Add files via upload

--- TV.java
+++ TV.java
@@ -1,53 +1,16 @@
 
-public class TV {
-	int channel = 1;
-	int volumeLevel = 1;
-	boolean on = true;
+public class TV extends Electronics {
 
-	public TV() {
+	int size;
+
+	/** Creates a new instance of TV */
+	public TV(double regularPrice, String manufacturer, int size) {
+		super(regularPrice, manufacturer);
+		this.size = size;
 	}
 
-	public void turnOn() {
-		on = true;
-	}
-
-	public void turnOff() {
-		on = false;
-	}
-
-	public void setChannel(int newChannel) {
-		if (newChannel > 0 && newChannel < 121) {
-			channel = newChannel;
-		}
-	}
-
-	public void setVolume(int newVolumeLevel) {
-		if (newVolumeLevel > 0 && newVolumeLevel < 8) {
-			volumeLevel = newVolumeLevel;
-		}
-	}
-
-	public void channelUp() {
-		if (channel < 120) {
-			channel++;
-		}
-	}
-
-	public void channelDown() {
-		if (channel > 1) {
-			channel--;
-		}
-	}
-
-	public void volumeUp() {
-		if (volumeLevel < 7) {
-			volumeLevel++;
-		}
-	}
-
-	public void volumeDown() {
-		if (volumeLevel > 1) {
-			volumeLevel--;
-		}
+	// Override this method
+	public double computeSalePrice() {
+		return super.getRegularPrice() * 0.8;
 	}
 }
